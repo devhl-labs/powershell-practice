11227102fd819a51cc450a1fc24b209303fdc19e
devhl-labs <shawnkanyer@gmail.com>
2020-08-10 13:55:26 -0400

    auto committed from auto-committing-setup.ps1!

---
+++ auto-committing-setup.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = "Stop"
+$env:GIT_REDIRECT_STDERR = '2>&1'  #https://github.com/dahlbyk/posh-git/issues/109
+
+# declare variables
+$GitHubUsername='devhl-labs'
+$PracticeRepoDir='E:\repos\powershell-practice'
+$CommitMessage='auto committed from auto-committing-setup.ps1!'
+
+# fork and clone this repo into the powershell-studio directory
+git clone "https://github.com/$GitHubUsername/powershell-practice" "$PracticeRepoDir"
+
+# TODO: complete steps 5-9
+Copy-Item -Path $PSCommandPath -Destination "$PracticeRepoDir"
+
+cd "$PracticeRepoDir"
+
+git add .
+
+git commit -a -m $CommitMessage
+
+git push origin master
